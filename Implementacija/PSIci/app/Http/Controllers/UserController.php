4bee91f16dbdbe21648d6bd5dee65aedf0694764
tjovanovic96 <tjovanovic510@gmail.com>
2018-05-31 18:47:13 +0200

    funkcionalnost uklanjanje naloga

--- Implementacija/PSIci/app/Http/Controllers/UserController.php
+++ Implementacija/PSIci/app/Http/Controllers/UserController.php
@@ -3,10 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Comment;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
     public function remove($id){
-
+        $user = User::find($id);
+        DB::beginTransaction();
+        DB::table('comments')->where('user_id','=',$id)->delete();
+        DB::table('watched_seasons')->where('user_id','=',$id)->delete();
+        DB::table('watched_episodes')->where('user_id','=',$id)->delete();
+        DB::commit();
+        $ratings = $user->ratings;
+        foreach($ratings as $rating){
+            $content_id = $rating->content_id;
+            $content = Content::find($content_id);
+            DB::beginTransaction();
+            DB::table('ratings')->where('user_id','=', $id)->where('content_id', '=',$content_id)->delete();
+            DB::commit();
+            $content->setRating;
+        }
+        Auth::logout();
+        DB::table('users')->where('username', '=',$id)->delete();
+        return view('home.index');
     }
 }
