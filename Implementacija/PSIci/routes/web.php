87b54a8de04f04208d1ff49f425b66ed3dcd6df0
simovic96 <aleksasimovic@rocketmail.com>
2018-06-01 11:31:09 +0200

    ispravka ruta

--- Implementacija/PSIci/routes/web.php
+++ Implementacija/PSIci/routes/web.php
@@ -27,10 +27,6 @@ Route::get('/series/{id}', 'GuestController@showSeries')->name('showseries');
 Route::get('episode/{id}','GuestController@showEpisode');
 
 
-//TIJANA DODAJ NAZIV KONTROLERA KOJI PRIKAZUJE EPIZODU!!!!!
-
-
-Route::get('episode/{content_id}','');
 
 
 
