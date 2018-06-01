4b9caf669f0064a5cd1f69096f50cb389e3d2331
tjovanovic96 <tjovanovic510@gmail.com>
2018-06-01 21:07:17 +0200

    t

--- Implementacija/PSIci/app/Http/Controllers/GuestController.php
+++ Implementacija/PSIci/app/Http/Controllers/GuestController.php
@@ -19,7 +19,7 @@ class GuestController extends Controller
         $content=Content::find($id);
         $seasons = $series->seasons;
         $type = 'series';
-        $contents = DB::table('contents')->join('seasons')->where('seasons.tvshow_id', '=', $series->content_id)->where('contents.id', '=', 'seasons.content_id')->select('contents.*')->orderBy('contents.id');
+        $contents = DB::table('contents')->join('seasons','seasons.content_id','=','contents.id')->where('seasons.tvshow_id', '=', $series->content_id)->where('contents.id', '=', 'seasons.content_id')->select('contents.*')->orderBy('contents.id');
         return view('content.tvshow', compact(['series', 'content', 'seasons', 'contents', 'type']));
     }
 
@@ -28,7 +28,7 @@ class GuestController extends Controller
         $content = Content::find($id);
         $episodes = $season->episodes;
         $type = 'season';
-        $contents = DB::table('contents')->join('episodes')->where('episodes.season_id','=', $season->content_id)->where('contents.id','=','episodes.content_id')->select('contents.*')->orderBy('contents.id');
+        $contents = DB::table('contents')->join('episodes','contents.id','=','episodes.content_id')->where('episodes.season_id','=', $season->content_id)->where('contents.id','=','episodes.content_id')->select('contents.*')->orderBy('contents.id');
         return view('content.season', compact(['season', 'content', 'episodes', 'contents', 'type']));
     }
 
