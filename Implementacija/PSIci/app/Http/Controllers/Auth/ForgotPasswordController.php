2d7f495be17f2b5968c072afa192cee7bf515177
Milica Despotovic <mdespotovic333@gmail.com>
2018-06-06 16:47:25 +0200

    komentari

--- Implementacija/PSIci/app/Http/Controllers/Auth/ForgotPasswordController.php
+++ Implementacija/PSIci/app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -8,14 +8,14 @@ use Illuminate\Http\Request;
 use  Illuminate\Support\Facades\DB;
 class ForgotPasswordController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reset Controller
-    |--------------------------------------------------------------------------
+    /*Author: Despotović Milica
+    |-------------------------------------------------------------------------|
+    | Password Reset Controller                                               |
+    |-------------------------------------------------------------------------|
+    |  
     |
-    | This controller is responsible for handling password reset emails and
-    | includes a trait which assists in sending these notifications from
-    | your application to your users. Feel free to explore this trait.
+    |
+    |
     |
     */
 
